0d5f262800bab7e33df535726c47dc78a4c99a1b
Salwa Eka Afiani <149504774+salwaekaafianii@users.noreply.github.com>
2026-06-06 19:37:30 +0700

    feat: implement OTP authentication flow

--- lib/app/modules/speech/controllers/speech_controller.dart
+++ lib/app/modules/speech/controllers/speech_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:kotoba_app/app/routes/app_pages.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:string_similarity/string_similarity.dart';
 import 'package:flutter_tts/flutter_tts.dart';
