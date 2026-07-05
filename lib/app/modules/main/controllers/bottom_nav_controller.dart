9349aaed0d1e465460d90b98e77731cb38d27de0
Ranifa <145567559+RanifaFitriyana@users.noreply.github.com>
2026-07-05 09:31:31 +0700

    fix: refresh home XP progress after quiz completion

--- lib/app/modules/main/controllers/bottom_nav_controller.dart
+++ lib/app/modules/main/controllers/bottom_nav_controller.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
+import '../../home/controllers/home_controller.dart';
 
 class BottomNavController extends GetxController {
   var currentIndex = 0.obs;
 
   void changeIndex(int index) {
     currentIndex.value = index;
+
+    if (index == 0 && Get.isRegistered<HomeController>()) {
+      Get.find<HomeController>().loadProfile();
+    }
   }
 
   void goToCamera() {
